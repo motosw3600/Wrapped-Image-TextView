d0e507910288f637d0dfa878d436463c71640a1b
motosw3600 <motosw3600@gmail.com>
2022-10-10 17:48:57 +0900

    feat: WrappedImageTextView

--- WrappedImageTextView/WrappedImageTextView/ViewController.swift
+++ WrappedImageTextView/WrappedImageTextView/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     init() {
@@ -16,9 +17,27 @@ class ViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private let wrappedImageTextView = WrappedImageTextView(image: UIImage(named: "flower"),
+                                                            align: .left,
+                                                            imageSize: CGSize(width: 200, height: 200),
+                                                            text: "dsadfwerqwersadfknvihiwlearnilasnfkdlnewklrnkqlwdsadfwerqwersadfknvihiwlearnilasnfkdlnewklrnkqlwdsadfwerqwersadfknvihiwlearnilasnfkdlnewklrnkqlwdsadfwerqwersadfknvihiwlearnilasnfkdlnewklrnkqlwdsadfwerqwersadfknvihiwlearnilasnfkdlnewklrnkqlwdsadfwerqwersadfknvihiwlearnilasnfkdlnewklrnkqlw"
+    )
+    
     override func viewDidLoad() {
         view.backgroundColor = .white
         super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        view.addSubview(wrappedImageTextView)
+        
+        wrappedImageTextView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(40)
+            make.leading.equalToSuperview().offset(40)
+            make.trailing.equalToSuperview().offset(-40)
+        }
     }
 
 }
--- WrappedImageTextView/WrappedImageTextView/WrappedImageTextView.swift
+++ WrappedImageTextView/WrappedImageTextView/WrappedImageTextView.swift
@@ -6,13 +6,68 @@
 //
 
 import UIKit
+import SnapKit
 
 class WrappedImageTextView: UIView {
-    init() {
+    enum ImageAlign {
+        case left
+        case right
+    }
+    
+    private let imageView = UIImageView()
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.isScrollEnabled = false
+        textView.font = .systemFont(ofSize: 16)
+        textView.textColor = .black
+        textView.isEditable = false
+        return textView
+    }()
+    private let imageSize: CGSize
+    private let align: ImageAlign
+    
+    init(image: UIImage?, align: ImageAlign, imageSize: CGSize, text: String) {
+        self.imageSize = imageSize
+        self.align = align
         super.init(frame: .zero)
+
+        imageView.image = image
+        
+        var bezierPath = UIBezierPath()
+        switch align {
+        case .left:
+            bezierPath = UIBezierPath(rect: CGRect(origin: CGPoint(x: 0, y: 0), size: imageSize))
+        case .right:
+            bezierPath = UIBezierPath(rect: CGRect(origin: CGPoint(x: UIScreen.main.bounds.width - 40 - 40 - imageSize.width, y: 0), size: imageSize))
+        }
+        textView.textContainer.exclusionPaths = [bezierPath]
+        textView.text = text
+        
+        setupView()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupView() {
+        addSubview(textView)
+        addSubview(imageView)
+        
+        textView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            switch align {
+            case .left:
+                make.leading.equalToSuperview()
+            case .right:
+                make.trailing.equalToSuperview()
+            }
+            
+            make.width.equalTo(imageSize.width)
+            make.height.equalTo(imageSize.height)
+        }
     }
 }
