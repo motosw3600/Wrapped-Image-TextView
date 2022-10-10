95403883794661db7ca1937c747641a99fe7e2f7
motosw3600 <motosw3600@gmail.com>
2022-10-10 16:37:54 +0900

    chore: 폴더구조 변경

--- WrappedImageTextView/WrappedImageTextView/ViewController.swift
+++ WrappedImageTextView/WrappedImageTextView/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    init() {
+        super.init(nibName: nil, bundle: nil)
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        super.viewDidLoad()
+    }
 
 }
 
---
+++ WrappedImageTextView/WrappedImageTextView/WrappedImageTextView.swift
@@ -0,0 +1,18 @@
+//
+//  WrappedImageTextView.swift
+//  WrappedImageTextView
+//
+//  Created by 박상우 on 2022/10/10.
+//
+
+import UIKit
+
+class WrappedImageTextView: UIView {
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
